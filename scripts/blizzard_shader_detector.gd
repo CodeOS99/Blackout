49f04762f71194e5b9422b57e6b9b35bfe50eaf0
Kushagra Sharma <kush.kuhu@gmail.com>
2025-11-30 10:59:12 +0530

    Added a blizzard bg noise

--- scripts/blizzard_shader_detector.gd
+++ scripts/blizzard_shader_detector.gd
@@ -2,6 +2,8 @@ extends Area3D
 
 func _on_body_exited(body: Node3D) -> void:
 	Globals.player.blizzard_shader_rect.visible = true
+	$"../BlizzardBg".volume_db = -12
 
 func _on_body_entered(body: Node3D) -> void:
 	Globals.player.blizzard_shader_rect.visible = false
+	$"../BlizzardBg".volume_db =  -20
